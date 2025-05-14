069af61213bba9cf0c7b48070b376a0f5e914adf
titomundo <tito.alberto.mundo@gmail.com>
2025-05-14 13:12:19 -0600

    initial commit

---
+++ adbDisableApps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+declare -a arr=(
+    "com.google.android.youtube"
+    "com.google.android.apps.youtube.music"
+    "com.android.chrome"
+    "com.google.android.apps.docs"
+    "com.google.android.calendar"
+    "com.google.android.gm"
+    "com.google.android.googlequicksearchbox"
+    "com.google.android.apps.subscriptions.red"
+    "com.google.android.apps.tachyon"
+    "com.google.android.videos"
+    "com.motorola.gamemode"
+    "com.google.android.apps.googleassistant"
+    "com.android.vending"
+    "com.motorola.ccc.notification"
+    "com.motorola.securityhub"
+    "com.google.android.apps.nbu.files"
+)
+
+for i in "${arr[@]}"
+do
+    #echo "$i"
+    adb shell pm disable-user --user 0 "$i" 
+done
